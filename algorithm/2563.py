dcf2ccd8806fd618e7778b4c110c5b434f10cfb6
solmysoul1 <solmysoul11@gmail.com>
2024-02-13 22:44:43 +0900

    practice

--- algorithm/2563.py
+++ algorithm/2563.py
@@ -1,16 +1,16 @@
 # 색종이
 paper = [[0]*100 for _ in range(100)]
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(T):
     N, M = map(int, input().split())
-    for i in range(99-M, 91-M):
-        for j in range(N, 10+N):
-            if 0<=i<100 and 0<=j<100:
-                paper[i][j] += 1
-    cnt = 0
-    for i in range(100):
-        for j in range(100):
-            if paper[i][j] == T:
-                cnt += 1
+    for i in range(N, N+10):
+        for j in range(M, M+10):
+            paper[i][j] = 1
+cnt = 0
+for n in range(100):
+    for m in range(100):
+        if paper[n][m] == 1:
+            cnt += 1
 
 print(cnt)
+
