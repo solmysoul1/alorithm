e1a45cc7c21046e4f2bdaa5273455c96f92afdff
solmysoul1 <solmysoul11@gmail.com>
2024-02-14 17:55:22 +0900

    algorithm

---
+++ algorithm/1978.py
@@ -0,0 +1,12 @@
+T = int(input())
+arr = list(map(int, input().split()))
+result = 0
+for i in arr:
+    cnt = 0
+    for j in range(1,i+1):
+        if i%j == 0:
+            cnt += 1
+    if cnt == 2:
+        result += 1
+print(result)
+    
---
+++ algorithm/4153.py
@@ -0,0 +1,12 @@
+# 직각삼각형
+
+
+while True:
+    arr = list(map(int, input().split()))
+    if arr[0] == 0 and arr[1] == 0 and arr[2] == 0:
+        break
+    arr.sort()
+    if arr[0]**2 + arr[1]**2 == arr[2]**2:
+        print('right')
+    else:
+        print('wrong')
