5b4aff19f4f4f60f6ed60a89e6c00329b120bf2f
solmysoul1 <solmysoul11@gmail.com>
2024-02-28 00:23:04 +0900

    practice

--- swea/5188.py
+++ swea/5188.py
@@ -28,6 +28,5 @@ for tc in range(1, T+1):
 
     print(f'#{tc} {result}')
     
-    
-    
-    
+
+   
--- swea/5189.py
+++ swea/5189.py
@@ -29,38 +29,74 @@
 #     EN(0)
 #     print(f'#{tc} {min_value}')
 
-def f1(i,k):
-    global min_v
-    if i == k:
-        # p 순서로 방문했을 때의 비용
-        s = e[0][p[0]-1] # 사무실에서 첫번째 관리구역
-        for j in range(1,k): # 관리구역 사이
-            s += e[p[j-1]-1][p[j]-1]
-        s += e[p[k-1]-1][0]
-        if min_v > s:
-            min_v = s
+# ----------------------------------------------------------------
 
+# def f1(i,k):
+#     global min_v
+#     if i == k:
+#         # p 순서로 방문했을 때의 비용
+#         s = e[0][p[0]-1] # 사무실에서 첫번째 관리구역
+#         for j in range(1,k): # 관리구역 사이
+#             s += e[p[j-1]-1][p[j]-1]
+#         s += e[p[k-1]-1][0]
+#         if min_v > s:
+#             min_v = s
+
+#     else:
+#         for j in range(k):
+#             if used[j] == 0:
+#                 used[j] = 1
+#                 p[i] = arr[j]
+#                 f1(i+1,k)
+#                 used[j] = 0
+
+# T = int(input())
+# for tc in range(1, T+1):
+#     N =int(input())
+#     e = [list(map(int, input().split())) for _ in range(N)]
+#     min_v = 1000
+#     # 방문 순서를 순열로
+#     arr = [i for i in range(2, N+1)]
+#     # 사무실 제외
+#     used = [0]*(N-1) 
+#     p = [0]*(N-1)
+#     f1(0,N-1)
+
+#     print(f'#{tc} {min_v}')
+
+path = []
+
+def e(x,y):
+    if x == y:
+        s = arr[0][path[0]-1] + arr[path[-1]-1][0]
+        for j in range(1,y):
+            s += arr[path[j-1]-1][path[j]-1]
+        total.append(s)
+        return
+    
     else:
-        for j in range(k):
-            if used[j] == 0:
-                used[j] = 1
-                p[i] = arr[j]
-                f1(i+1,k)
-                used[j] = 0
+        for i in range(2,N+1):
+            if visited[i] == 0:
+                visited[i] = 1
+                path.append(i)
+                e(x+1,y)
+                path.pop()
+                visited[i] = 0
+    
 
 T = int(input())
 for tc in range(1, T+1):
-    N =int(input())
-    e = [list(map(int, input().split())) for _ in range(N)]
-    min_v = 1000
-    # 방문 순서를 순열로
-    arr = [i for i in range(2, N+1)]
-    # 사무실 제외
-    used = [0]*(N-1) 
-    p = [0]*(N-1)
-    f1(0,N-1)
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    total = []
+    visited = [0]*(N+1)
+    e(0,N-1)
+    min_value = min(total)
 
-    print(f'#{tc} {min_v}')
+    print(f'{tc} {min_value}')
+    
+
+
 
 
 
