23956e3e72699dd337a0d7c67e93f09d8bcc045b
solmysoul <solmysoul11@gmail.com>
2024-03-14 17:21:18 +0900

    add code

--- backjoon/14501.py
+++ backjoon/14501.py
@@ -48,4 +48,21 @@
 #         dp[i] = max_v
 # print(max_v)
 
-        
+# ----------------------------------
+
+# def dfs(k, pay):
+#     global ans
+#     if k >= N:
+#         ans = max(ans, pay)
+#         return
+#     if k + arr[k][0] <= N:
+#         dfs(k + arr[k][0], pay + arr[k][1])
+#     dfs(k+1, pay)
+
+# N = int(input())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+# print(arr)
+# ans = 0
+# dfs(0,0)
+# print(ans)
+
